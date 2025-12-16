f958b3bb2e6de444edbccd6ac919b43754977ba8
Abrham Aragie <145662971+admarspia@users.noreply.github.com>
2025-12-16 16:48:31 +0300

    Clean up comments in index.js
    Removed comments related to React 18 root creation.

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,12 +5,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
-// Create a root for React 18 and render the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     {" "}
-    {/* Wrap the entire app with Router */}
     <App />
   </Router>
 );
